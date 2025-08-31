e1fd362152fd7d8a4a45ea8a8d27033155d2b076
George Tsendra <george.tsendra66@gmail.com>
2025-08-31 21:13:24 +0200

    docs

--- src/utils/configInstance.ts
+++ src/utils/configInstance.ts
@@ -1,4 +1,4 @@
 export const config = {
-  NODE_ENV: process.env.NODE_ENV,
-  REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+  NODE_ENV: process.env.NODE_ENV, //development
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL, //http://localhost:3000/api
 };
